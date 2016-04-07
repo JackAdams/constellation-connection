b8280fac1fc3a15a5841220e56e8ab406ae574e1
Brent Abrahams <brent_abrahams@yahoo.com>
2016-04-07 21:20:19 +0800

    first commit

---
+++ connection.js
@@ -0,0 +1,28 @@
+// Hook in to constellation UI
+
+var Constellation = Package["constellation:console"].API;
+    
+Constellation.addTab({
+  name: 'Connection',
+  headerContentTemplate: 'Constellation_connection',
+  noOpen:true,
+  onClick: "toggleConnection"
+});
+
+Constellation.registerCallbacks({
+  toggleConnection : function () {
+	if (Meteor.status().connected) {
+	  Meteor.disconnect();	
+	}
+	else {
+	  Meteor.reconnect();	
+	}
+  }
+});
+
+Template.Constellation_tiny.helpers({
+  connection: function () {
+    return Meteor.status().connected;  
+  }
+});
+	
---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'constellation:connection',
+  version: '0.4.0',
+  summary: 'Connection status plugin for Constellation',
+  git: 'https://github.com/JackAdams/constellation-connection.git',
+  documentation: 'README.md',
+  debugOnly: true
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+
+  api.use(['templating','reactive-dict','tracker'], 'client');
+  api.use('constellation:console@1.4.0', 'client');
+
+  api.addFiles('connection.css','client');
+  api.addFiles('connection.html','client');
+  api.addFiles('connection.js','client');
+  
+  api.imply('constellation:console');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+});
